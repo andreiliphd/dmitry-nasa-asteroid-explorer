7ce9af70bb8924e72a5a0439801c57793a91b2ed
Andrei Li <al@skytech.app>
2021-07-10 19:31:03 +0300

    Initial commit.

---
+++ app/src/main/java/com/udacity/asteroidradar/database/Asteroid.kt
@@ -0,0 +1,26 @@
+package com.udacity.asteroidradar.database
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+@Entity(tableName = "asteroids_table")
+data class Asteroid(@PrimaryKey(autoGenerate = true)
+                    val id: Long,
+                    @ColumnInfo(name = "codename")
+                    val codename: String,
+                    @ColumnInfo(name = "closeApproachDate")
+                    val closeApproachDate: String,
+                    @ColumnInfo(name = "absoluteMagnitude")
+                    val absoluteMagnitude: Double,
+                    @ColumnInfo(name = "estimatedDiameter")
+                    val estimatedDiameter: Double,
+                    @ColumnInfo(name = "relativeVelocity")
+                    val relativeVelocity: Double,
+                    @ColumnInfo(name = "distanceFromEarth")
+                    val distanceFromEarth: Double,
+                    @ColumnInfo(name = "isPotentiallyHazardous")
+                    val isPotentiallyHazardous: Boolean) : Parcelable
