b3a2e03e694a839e92b1e1b21ad28b0c41a52d09
Andrei Li <al@skytech.app>
2021-08-21 21:36:34 +0300

    ga installed

--- app/src/main/java/com/andreiliphd/asteroidradar/MainActivity.kt
+++ app/src/main/java/com/andreiliphd/asteroidradar/MainActivity.kt
@@ -5,14 +5,24 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.analytics.ktx.logEvent
+import com.google.firebase.ktx.Firebase
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        firebaseAnalytics = Firebase.analytics
         setContentView(R.layout.activity_main)
-
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_ITEM) {
+            param(FirebaseAnalytics.Param.ITEM_ID, "test id")
+            param(FirebaseAnalytics.Param.ITEM_NAME, "andrei li")
+            param(FirebaseAnalytics.Param.CONTENT_TYPE, "image")
+        }
         val crashButton = Button(this)
         crashButton.text = "Test Crash"
         crashButton.setOnClickListener {
