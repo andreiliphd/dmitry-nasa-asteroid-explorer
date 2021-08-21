59034b9a02a87289d99482765546c8853227a394
Andrei Li <al@skytech.app>
2021-08-21 21:17:32 +0300

    crash button

--- app/src/main/java/com/andreiliphd/asteroidradar/MainActivity.kt
+++ app/src/main/java/com/andreiliphd/asteroidradar/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.andreiliphd.asteroidradar
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val crashButton = Button(this)
+        crashButton.text = "Test Crash"
+        crashButton.setOnClickListener {
+            throw RuntimeException("Test Crash") // Force a crash
+        }
+
+        addContentView(crashButton, ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT))
+
     }
 }
